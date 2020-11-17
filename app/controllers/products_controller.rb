95c535ca046c4226135b1eea42436be5a9743f89
Jessica Chan <jwinchan@gmail.com>
2020-11-17 15:47:50 -0800

    created new and create methods for product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,6 @@
 class ProductsController < ApplicationController
+  before_action :find_product, only [:show, :edit, :update]
+
   def index
   end
 
@@ -6,8 +8,31 @@ class ProductsController < ApplicationController
   end
 
   def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      flash[:success = "Product has been successfully added"
+      redirect_to product_path(@product.id)
+    else
+      flash[:error] = "Product has not been added"
+      render :new, status: :bad_request
+      return
+    end
   end
 
   def edit
+  end
+
+  private
+
+  def find_product
+    @product = Product.find_by_id(params[:id])
+  end
+
+  def product_params
+    return params.require(:product).permit(:name, :stock, :price, :description, :photo_url, :user_id)
   end
 end
