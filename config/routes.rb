738f705ed211c11acb09961e54bffb0de461ee3e
Ting-Yi Liu <46391352+ichbinorange@users.noreply.github.com>
2020-11-17 16:04:01 -0800

    revise routes to include destroy action

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/show'
-  get 'users/edit'
-  get 'products/index'
-  get 'products/show'
-  get 'products/new'
-  get 'products/edit'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users, except: [:new]
+  resources :products
 end
