2dc1c514d4c2bd418cb8aa4e9b3815e1c0be6528
Maha-ElMais <mahaelmais@gmail.com>
2020-11-17 19:08:51 -0800

    edit update product and view

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -9,5 +9,36 @@ class ProductsController < ApplicationController
   end
 
   def edit
+    @product = Product.find_by(id: params[:id])
+    driver_id = params[:id]
+
+    begin
+      @product = Product.find(driver_id)
+    rescue ActiveRecord::RecordNotFound
+      @product = nil
+    end
+
+    if @product.nil?
+      head :not_found
+      return
+    end
+  end
+
+  def update
+    @product = Product.find_by(id: params[:id])
+    if @product.nil?
+      head :not_found
+      return
+    elsif @product.update(
+        name: params[:product][:name],
+        vin: params[:product][:vin],
+        available: params[:product][:available]
+    )
+      redirect_to driver_path # go to the product details page
+      return
+    else # save failed
+    render :edit, status: :bad_request # show the new product form view again
+    return
+    end
   end
 end
