4694beb79b0ff19cd1de5321f944a8a4f77ce929
Jessica Chan <jwinchan@gmail.com>
2020-11-17 18:52:58 -0800

    added edit and  update methods for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :find_user, only: [:show, :edit, :update]
+
   def index
   end
 
@@ -6,5 +8,37 @@ class UsersController < ApplicationController
   end
 
   def edit
+    if @user.nil?
+      flash[:error] = "User not found"
+      redirect_to users_path
+      return
+    end
+  end
+
+  def update
+    if @user.nil?
+      flash[:error] = "User not found"
+      redirect_to users_path
+      return
+    elsif @user.update(user_params)
+      flash[:success] = "User updated successfully"
+      redirect_to user_path
+      return
+    else
+      flash.now[:error] = "Something happened. User not updated."
+      render :edit, status: :bad_request
+      return
+    end
+  end
+
+
+  private
+
+  def find_user
+    @user = User.find_by_id(params[:id])
+  end
+
+  def user_params
+    return params.require(:user).permit(:name, :description, :uid, :provider, :email)
   end
 end
