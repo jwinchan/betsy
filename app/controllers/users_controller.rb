9b86ed8f97382b58138ad792a909379c3fe4eb12
Ting-Yi Liu <46391352+ichbinorange@users.noreply.github.com>
2020-11-17 17:33:54 -0800

    add index and show action for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,14 @@
 class UsersController < ApplicationController
   def index
+    @users = User.all
   end
-
+  
   def show
+    @user = User.find_by(id: params[:id])
+
+    if @user.nil?
+      render :file => "#{Rails.root}/public/404.html",  layout: false, status: :not_found
+    end
   end
 
   def edit
