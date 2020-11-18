3adb6314049bce00bafdcc5ddb8ba207b0cf1435
Marj E <marj@marjorieetienne.com>
2020-11-17 16:33:57 -0800

    fixed index

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -1,27 +1,27 @@
 require "test_helper"
 
 describe ProductsController do
-  # let (:product) {
-  #   Product.create name: "sample product"
-  # }
+  let (:product) {
+    Product.create name: "sample product"
+  }
   describe "index" do
     it "must get index" do
       get products_index_url
       must_respond_with :success
     end
 
-    # it "responds with success when there are many products saved" do
-    #   product
-    #   get products_path
-    #   expect(Product.count).must_equal 1
-    #   must_respond_with :success
-    # end
-    #
-    # it "responds with success when there are no products saved" do
-    #   get products_path
-    #   expect(Product.count).must_equal 0
-    #   must_respond_with :success
-    # end
+    it "responds with success when there are many products saved" do
+      product
+      get products_index_url
+      expect(Product.count).must_equal 1
+      must_respond_with :success
+    end
+
+    it "responds with success when there are no products saved" do
+      get products_index_url
+      expect(Product.count).must_equal 0
+      must_respond_with :success
+    end
   end
 
   it "must get show" do
