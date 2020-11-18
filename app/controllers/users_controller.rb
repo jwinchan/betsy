5cd9fb94d2d51a6b905001d0dcbffec72ae23da0
Ting-Yi Liu <46391352+ichbinorange@users.noreply.github.com>
2020-11-17 17:55:44 -0800

    remove index action and revise show action for user to include the feature that user cannot view other user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,14 @@
 class UsersController < ApplicationController
-  def index
-    @users = User.all
-  end
-  
   def show
     @user = User.find_by(id: params[:id])
 
     if @user.nil?
       render :file => "#{Rails.root}/public/404.html",  layout: false, status: :not_found
+    elsif session[:id] != @user.id  # May need to change to current user
+      flash[:error] = "You cannot see other users' information."
+      # need to clarify which path to redirect
+      redirect_to user_path(@user)
+      return
     end
   end
 
