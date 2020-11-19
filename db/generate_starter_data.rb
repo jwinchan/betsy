94bafee607db38f8bcbe5ba128a6d86661fe6960
Maha-ElMais <mahaelmais@gmail.com>
2020-11-18 19:02:32 -0800

    generated  starter data and created a seed.rb file

---
+++ db/generate_starter_data.rb
@@ -0,0 +1,27 @@
+require "faker"
+require "date"
+require "csv"
+
+# we already provide a filled out works_seeds.csv file, but feel free to
+# run this script in order to replace it and generate a new one
+# run using the command:
+# $ ruby db/generate_seeds.rb
+# if satisfied with this new works_seeds.csv file, recreate the db with:
+# $ rails db:reset
+# doesn't currently check for if titles are unique against each other
+
+CSV.open("db/seed_data/products_seeds.csv", "w", :write_headers => true,
+         :headers => ["name", "stock", "price", "description", "user_id", "photo_url", "retired"]) do |csv|
+  25.times do
+    name = Faker::Name.name
+    stock = rand(0..500)
+    price =  rand(100..100000)
+    description = Faker::Lorem.sentence
+    user_id = rand(0..10)
+    photo_url = Faker::Avatar.image
+    retired = %w(true false).sample
+
+    csv << [name, stock, price, description, user_id, photo_url, retired]
+  end
+end
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require "csv"
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +6,37 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+PRODUCT_FILE = Rails.root.join('db', 'seed_data', 'products_seeds.csv')
+puts "Loading raw product data from #{PRODUCT_FILE}"
+
+product_failures = []
+CSV.foreach(PRODUCT_FILE, :headers => true) do |row|
+  product = Product.new
+  product.name = row['name']
+  product.stock = row['stock']
+  product.price = row['price']
+  product.description = row['description']
+  product.user_id = row['user_id']
+  product.photo_url = row['photo_url']
+  product.retired = row['retired']
+  successful = product.save
+  if !successful
+    product_failures << product
+    puts "Failed to save product: #{product.inspect}"
+  else
+    puts "Created product: #{product.inspect}"
+  end
+end
+
+puts "Added #{Product.count} product records"
+puts "#{product_failures.length} products failed to save"
+
+# Since we set the primary key (the ID) manually on each of the
+# tables, we've got to tell postgres to reload the latest ID
+# values. Otherwise when we create a new record it will try
+# to start at ID 1, which will be a conflict.
+puts "Manually resetting PK sequence on each table"
+ActiveRecord::Base.connection.tables.each do |t|
+  ActiveRecord::Base.connection.reset_pk_sequence!(t)
+end
+puts "done"
