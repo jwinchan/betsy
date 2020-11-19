f719e9cb3c8c7f7b80cfb8c3a85469450e05d3ed
Jessica Chan <jwinchan@gmail.com>
2020-11-19 04:48:20 -0800

    added html/css and corrected through relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_many :products
-  has_many :orderitems, through :products
-  has_many :categories, through :products
+  has_many :orderitems, through: :products
+  has_many :categories, through: :products
 
   def self.build_from_github(auth_hash)
     user = User.new
