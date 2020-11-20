0b7f00b73aa4681820fc7ec7f1be1c772024b888
Ting-Yi Liu <46391352+ichbinorange@users.noreply.github.com>
2020-11-19 22:04:50 -0800

    add shipped & cancelled to orderitems table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_20_030300) do
+ActiveRecord::Schema.define(version: 2020_11_20_050820) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,8 @@ ActiveRecord::Schema.define(version: 2020_11_20_030300) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "order_status", default: "pending"
+    t.boolean "shipped", default: false
+    t.boolean "cancelled", default: false
   end
 
   create_table "orders", force: :cascade do |t|
