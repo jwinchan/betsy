052aeb68adde3e86469fdefec7f0467f9dcba4ae
Ting-Yi Liu <46391352+ichbinorange@users.noreply.github.com>
2020-11-19 22:12:13 -0800

    revise show action for order which only for confirmed order

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,28 +4,22 @@ class OrdersController < ApplicationController
   def cart
   end
 
-  def show
-    # May need different order status, order = true is ordered invoice, order = false is shopping cart, or add cart model & controller
+  def order_show
     @order = Order.find_by(id: params[:id])
-    @current_order = order_cart
     
     # For invoce
     if @order.nil?  
       render :file => "#{Rails.root}/public/404.html",  layout: false, status: :not_found
       return  
-    elsif session[:user_id] != @order.user_id # Need to add user_id in orders table
+    elsif @order.products.where(user_id: session[:user_id]).empty?
       flash[:success] = "You cannot see other users' order."
       # need to clarify which path to redirect
       redirect_to products_path
-      return
-    else
-      @order_show = @order 
-      redirect_to order_path(@order.id)
       return 
+    else
+      redirect_to order_path(@order.id)
+      return
     end
-    
-    # for shopping cart
-    @order_show = @current_order  
   end
 
 
