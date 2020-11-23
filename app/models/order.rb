a7461b7615e06788a7bed5449921a2ec39dbd4b3
Ting-Yi Liu <46391352+ichbinorange@users.noreply.github.com>
2020-11-22 23:35:19 -0800

    remove redundent validate

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,14 +1,13 @@
 class Order < ApplicationRecord
-  has_many :orderitems
-  has_many :products, through: :orderitems
+  has_many :orderitems, on: :create
+  has_many :products, through: :orderitems, on: :create
 
-  validate :orderitem_num?
-  validates :name, presence: true, on: :create,
+  validates :name, presence: true, on: :create
   validates :email, presence: true, 
                     format: { with: URI::MailTo::EMAIL_REGEXP, message: "must be a valid email"},
                     on: :create
-  validates :mailing_address, presence: true, on: :create,
-  validates :cc_name, presence: true, on: :create,
+  validates :mailing_address, presence: true, on: :create
+  validates :cc_name, presence: true, on: :create
   validates :cc_number, presence: true,  
                         numericality: { only_integer: true }, 
                         on: :create,
@@ -16,13 +15,7 @@ class Order < ApplicationRecord
   validates :cc_exp_date, presence: true, 
                           on: :create,  
                           cc_exp_date_valid?  
-  validates :billing_zip_code, presence: true, on: :create,
-
-  def orderitem_num?
-    if orderitems.size <= 0 
-      errors.add(:cc_number, "orderitems in an order must greater than 0")
-    end
-  end
+  validates :billing_zip_code, presence: true, on: :create
   
   def cc_num_valid?
     if cc_number.present? && (cc_number.to_s.length < 13 || cc_number.to_s.length > 19)
