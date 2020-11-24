58a2673fbe8fc35c756299d300ec5623913dbb8f
Maha-ElMais <mahaelmais@gmail.com>
2020-11-23 23:20:40 -0800

    added payment and complete actions to orders controller and custom methods to order model and updated routes

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,19 +1,19 @@
 class Order < ApplicationRecord
   has_many :orderitems
   has_many :products, through: :orderitems
-
-  validates :name, presence: true, on: :create
-  validates :email, presence: true, 
-                    format: { with: URI::MailTo::EMAIL_REGEXP, message: "must be a valid email"},
-                    on: :create
-  validates :mailing_address, presence: true, on: :create
-  validates :cc_name, presence: true, on: :create
-  validates :cc_number, presence: true,  
-                        numericality: { only_integer: true }, 
-                        on: :create
-  validates :cc_exp_date, presence: true, on: :create   
-  validates :billing_zip_code, presence: true, on: :create
-  validate :cc_num_valid?, :cc_exp_date_valid? 
+  #
+  # validates :name, presence: true, on: :create
+  # validates :email, presence: true,
+  #                   format: { with: URI::MailTo::EMAIL_REGEXP, message: "must be a valid email"},
+  #                   on: :create
+  # validates :mailing_address, presence: true, on: :create
+  # validates :cc_name, presence: true, on: :create
+  # validates :cc_number, presence: true,
+  #                       numericality: { only_integer: true },
+  #                       on: :create
+  # validates :cc_exp_date, presence: true, on: :create
+  # validates :billing_zip_code, presence: true, on: :create
+  # validate :cc_num_valid?, :cc_exp_date_valid?
   
   def cc_num_valid?
     if cc_number.present? && (cc_number.to_s.length < 13 || cc_number.to_s.length > 19)
@@ -24,6 +24,20 @@ class Order < ApplicationRecord
   def cc_exp_date_valid?
     if cc_exp_date.present? && Datetime.parse(cc_exp_date) < Date.today
       errors.add(:cc_exp_date, "can't be in the past")
+    end
+  end
+
+  def mark_as_paid
+    self.orderitems.each do |item|
+      item.order_status = "paid"
+      item.save
+    end
+  end
+
+  def update_stock
+    self.orderitems.each do |item|
+      item.product.stock -= item.quantity
+      item.product.save
     end
   end
 end
