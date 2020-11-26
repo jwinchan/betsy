9febbc36696a86165407da365bf4ac4db45b51af
Jessica Chan <jwinchan@gmail.com>
2020-11-26 11:01:26 -0800

    added more custom method tests for order

--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -14,18 +14,19 @@ describe Order do
   end
 
   describe "Custom Methods" do
-    it "marks orderitems as paid only when order payment is completed" do
+    before do
       @order = orders(:order1)
-
+    end
+    
+    it "marks orderitems as paid only when order payment is completed" do
       expect(@order.orderitems.first.order_status).must_equal "pending"
 
       @order.mark_as_paid
 
       expect(@order.orderitems.first.order_status).must_equal "paid"
     end
-    it "updates product stock only when order payment is completed" do
-      @order = orders(:order1)
 
+    it "updates product stock only when order payment is completed" do
       expect(@order.products.first.stock).must_equal 100
 
       @order.update_stock
@@ -34,7 +35,13 @@ describe Order do
     end
 
     it "sums up order subtotals" do
-      
+      expect(@order.total).must_equal 40005
+    end
+
+    it "only shows last 4 digits of credit card number" do
+      expect(@order.cc_number).must_equal 22221111
+
+      expect(@order.last_digits).must_equal "1111"
     end
   end
 end
