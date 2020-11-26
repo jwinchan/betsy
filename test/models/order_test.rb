ef244ae52a23704085c23fd4d8e3e664fb5a6eb0
Jessica Chan <jwinchan@gmail.com>
2020-11-26 10:28:30 -0800

    tested custom modek methods for order

--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -1,7 +1,40 @@
 require "test_helper"
 
 describe Order do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  describe "Relationships" do
+
+  end
+
+  describe "Validations" do
+
+  end
+
+  describe "Custom Validations" do
+
+  end
+
+  describe "Custom Methods" do
+    it "marks orderitems as paid only when order payment is completed" do
+      @order = orders(:order1)
+
+      expect(@order.orderitems.first.order_status).must_equal "pending"
+
+      @order.mark_as_paid
+
+      expect(@order.orderitems.first.order_status).must_equal "paid"
+    end
+    it "updates product stock only when order payment is completed" do
+      @order = orders(:order1)
+
+      expect(@order.products.first.stock).must_equal 100
+
+      @order.update_stock
+
+      expect(@order.products.first.stock).must_equal 99
+    end
+
+    it "sums up order subtotals" do
+      
+    end
+  end
 end
